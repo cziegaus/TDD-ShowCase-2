b8788c854cad10e12324a32f77929919f9807c14
cziegaus <54708551+cziegaus@users.noreply.github.com>
2021-03-11 19:51:20 +0100

    Initialer Commit für den neuen TDD-ShowCase

---
+++ src/test/java/TDDShowCaseTestPackage/TDDShowCaseTest.java
@@ -0,0 +1,21 @@
+package TDDShowCaseTestPackage;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class TDDShowCaseTest {
+
+    @Test
+    public void firstTest() {
+        System.out.println("Das ist der erste Test.");
+        assertTrue(true);
+    }
+
+    @Test
+    public void secondTest() {
+        System.out.println("Das ist der zweite Test.");
+        assertTrue(true);
+    }
+}
+
+
