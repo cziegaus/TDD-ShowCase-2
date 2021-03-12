6a64011402f777d1c1bd6bb3c2690fd292782733
cziegaus <54708551+cziegaus@users.noreply.github.com>
2021-03-12 12:36:27 +0100

    Andrea gezeigt

--- src/main/java/TDDShowCasePackage/TimePair.java
+++ src/main/java/TDDShowCasePackage/TimePair.java
@@ -1,18 +1,17 @@
 package TDDShowCasePackage;
 
-public class TimeDifference {
+public class TimePair {
 
     private String startTime, endTime;
 
-    public void TimeDifference(String strStartTime, String strEndTime) {
+    public void TimePair(String strStartTime, String strEndTime) {
     }
 
     public void setTimeValues (String startTime, String endTime) {
-
     }
 
     public double getTimeDifference() {
-        return 0.0;
+        return 1.0;
     }
 
 }
--- src/test/java/TDDShowCaseTestPackage/TDDShowCaseTest.java
+++ src/test/java/TDDShowCaseTestPackage/TDDShowCaseTest.java
@@ -6,11 +6,18 @@ import org.junit.Test;
 
 public class TDDShowCaseTest {
 
-    TimeDifference aTimeDifference = new TimeDifference();
+    TimePair aTimePair = new TimePair();
 
     @Test
     public void timeDifferenceTest_1Std() {
-        aTimeDifference.setTimeValues("10:00", "11:00");
-        assertEquals(1.0, aTimeDifference.getTimeDifference(), 0.0);
+        aTimePair.setTimeValues("10:00", "11:00");
+        assertEquals(1.0, aTimePair.getTimeDifference(), 0.0);
     }
+
+    //@Test
+    //public void timeDifferenceTest_0Std() {
+    //    aTimePair.setTimeValues("11:00", "11:00");
+    //    assertEquals(0.0, aTimePair.getTimeDifference(), 0.0);
+    //}
+
 }
